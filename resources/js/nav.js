45f213c6c3c896fadbea05d2c63536b48ba15692
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2023-11-04 00:52:09 -0300

    enlaces funcionando

--- resources/js/nav.js
+++ resources/js/nav.js
@@ -10,6 +10,7 @@
 
         navegacionFija();
         scrollNav();
+        enlaceActivo();
 
     }
 
@@ -17,7 +18,6 @@
         const barra = document.querySelector('.header__nav');
         const skills = document.querySelector('#skills');
         const header = document.querySelector('.header__overlay');
-        //const body = document.querySelector('body');
     
         window.addEventListener('scroll', function() {
 
@@ -40,8 +40,6 @@
         enlaces.forEach(enlace => {
             enlace.addEventListener('click', function(e) {
 
-                console.log(e);
-                //console.log(enlace);
                 e.preventDefault();
 
                 
@@ -50,6 +48,60 @@
                 seccion.scrollIntoView({ behavior: "smooth", block: "start"});
             });
         });
+    }
+
+    function enlaceActivo() {
+
+        // seleccionar enlaces
+        const enlaces = document.querySelectorAll('.header__nav a');
+
+        enlaces.forEach(enlace => {
+
+            const elemento = enlace.attributes.href.value;
+
+            const seccion = document.querySelector(elemento);
+
+
+            window.addEventListener('scroll', function() {
+
+            const rect = seccion.getBoundingClientRect();
+  
+            if (rect.top < window.innerHeight && rect.bottom >= 0) {
+              // El elemento está en la vista
+              enlace.classList.add('color-orange');
+            } else {
+              // El elemento ya no está en la vista
+              enlace.classList.remove('color-orange');
+            }
+        });
+
+            
+
+
+            // window.addEventListener('scroll', function() {
+
+            //     if( seccion.getBoundingClientRect().top < 0  && seccion.getBoundingClientRect().bottom < 0) {
+    
+
+            //         enlace.classList.add('color-orange');
+    
+            //     } 
+            //     else {
+            //         enlace.classList.remove('color-orange');
+            //     }
+    
+            // });
+
+
+
+
+        });
+
+
+
+        // seleccionar elementos / headings
+
+
     }
 
     
