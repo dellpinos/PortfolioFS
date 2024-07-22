a31ba0fcc7b2ae247ed13a98f5ee287135db6c7c
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-07-22 01:03:35 -0300

    responsive de proyectos principales

--- resources/views/home/dev-projects.blade.php
+++ resources/views/home/dev-projects.blade.php
@@ -14,11 +14,6 @@
 
                     <div class="dev-projects__card dev-projects__ticket">
                         <div class="dev-projects__contenedor-heading">
-
-                            {{-- <div class="dev-projects__contenedor-img">
-                                <img loading="lazy" src="img/logoTL_2.png" alt="Logo Tus Listas" width="200"
-                                    height="70">
-                            </div> --}}
                             <h3 class="dev-projects__heading-ta">TicketAdmin</h3>
 
                             <div class="dev-projects__iconos">
@@ -34,7 +29,7 @@
                                 <i class="dev-projects__icono--oculto c-rojo fa-brands fa-laravel"></i>
                                 <i class="dev-projects__icono--oculto c-ama fa-brands fa-square-js"></i>
                                 <i class="dev-projects__icono--oculto c-rosa fa-brands fa-sass"></i>
-                                <a href="https://github.com/dellpinos/tusListas" target="_blank">
+                                <a href="https://github.com/dellpinos" target="_blank">
                                     <i class="dev-projects__icono fa-brands fa-github"></i>
                                 </a>
                             </div>
@@ -50,7 +45,7 @@
                                 <iframe width="700" height="396" src="https://www.youtube.com/embed/UH6vv0EN7Bw" title="Recorrido rápido por la aplicación TusListas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </div> --}}
                             <div class="tour__contenedor-card">
-                                <div class="tour__card-ticket" id="tour-card">
+                                <div class="tour__card-ticket" id="tour-card-ticket">
                                     <i id="tour-play-icon" class="tour__play fa-solid fa-circle-play"></i>
 
                                     <img src="{{ asset('img/page.svg') }}" class="tour__card-logo-ta"
@@ -150,29 +145,14 @@
                 <div class="dev-projects__grid">
                     <div class="dev-projects__card dev-projects__stuff">
                         <div class="dev-projects__contenedor-heading">
-                            {{-- 
-                            <div class="dev-projects__contenedor-img">
-                                <img loading="lazy" src="img/logoTL_2.png" alt="Logo Tus Listas" width="200"
-                                    height="70">
-                            </div> --}}
                             <h3 class="dev-projects__heading-st">Stuff</h3>
 
                             <div class="dev-projects__iconos">
-                                {{-- <svg class="dev-projects__icono--oculto" xmlns="http://www.w3.org/2000/svg"
-                                    class="icon icon-tabler icon-tabler-brand-mysql" width="40" height="40"
-                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="#0284C7" fill="none"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path
-                                        d="M13 21c-1.427 -1.026 -3.59 -3.854 -4 -6c-.486 .77 -1.501 2 -2 2c-1.499 -.888 -.574 -3.973 0 -6c-1.596 -1.433 -2.468 -2.458 -2.5 -4c-3.35 -3.44 -.444 -5.27 2.5 -3h1c8.482 .5 6.421 8.07 9 11.5c2.295 .522 3.665 2.254 5 3.5c-2.086 -.2 -2.784 -.344 -3.5 0c.478 1.64 2.123 2.2 3.5 3" />
-                                    <path d="M9 7h.01" />
-                                </svg>
-                                <i class="dev-projects__icono--oculto c-rosa fa-brands fa-sass"></i> --}}
                                 <i class="dev-projects__icono--oculto c-rojo fa-brands fa-laravel"></i>
                                 <i class="dev-projects__icono--oculto c-ama fa-brands fa-square-js"></i>
                                 <i class="dev-projects__icono--oculto c-ama fa-brands fa-square-js"></i>
                                 <i class="dev-projects__icono--oculto c-ama fa-brands fa-square-js"></i>
-                                <a href="https://github.com/dellpinos/tusListas" target="_blank">
+                                <a href="https://github.com/dellpinos/stuffworks" target="_blank">
                                     <i class="dev-projects__icono fa-brands fa-github"></i>
                                 </a>
                             </div>
@@ -188,12 +168,12 @@
                                 <iframe width="700" height="396" src="https://www.youtube.com/embed/UH6vv0EN7Bw" title="Recorrido rápido por la aplicación TusListas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </div> --}}
                             <div class="tour__contenedor-card">
-                                <div class="tour__card-stuff" id="tour-card">
+                                <div class="tour__card-stuff" id="tour-card-stuff">
                                     <i id="tour-play-icon" class="tour__play fa-solid fa-circle-play"></i>
 
                                     {{-- <img src="{{ asset('img/LogoSinFondo.png') }}" class="tour__card-logo"
                                         alt="Logo Tus Listas" loading="lazy"> --}}
-                                    <i class="fa-solid fa-gears tour__card-logo"></i>
+                                    <i class="fa-solid fa-gears tour__card-logo-st"></i>
                                     <div class="tour__card-contenedor-text">
 
                                         <h4 class="tour__card-heading">TusListas<br> vista rápida</h4>
